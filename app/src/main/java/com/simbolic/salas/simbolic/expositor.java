1bc565df65d342595da6b520bec0e79988741916
salas <rafa.salas@gmail.com>
2016-07-21 13:59:35 +0200

    third Test

--- app/src/main/java/com/simbolic/salas/simbolic/expositor.java
+++ app/src/main/java/com/simbolic/salas/simbolic/expositor.java
@@ -3,8 +3,11 @@ package com.simbolic.salas.simbolic;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -21,14 +24,31 @@ public class expositor extends View {
     @Override
 
     protected void onDraw(Canvas canvas){
+
+        int width=getWidth();
+        int height=getHeight();
+        Rect pantalla;
+        pantalla=new Rect();
+
+
+
         Resources res = getResources();
-        String erpelo="pelo";
+        String erpelo="pelo1";
         int idpelo=res.getIdentifier(erpelo, "drawable", "com.simbolic.salas.simbolic");
-        VectorDrawable drawable = (VectorDrawable) res.getDrawable(R.drawable.test);
-        VectorDrawable drawable2 = (VectorDrawable) res.getDrawable(idpelo);
-        canvas.drawColor(0xFFFF0000);
-        drawable.setBounds(0,0,500,800);
-        drawable2.setBounds(20,20,100,100);
+        VectorDrawable drawable = (VectorDrawable) res.getDrawable(R.drawable.test1, null);
+        VectorDrawable drawable2 = (VectorDrawable) res.getDrawable(idpelo, null);
+        int draw_height=drawable.getIntrinsicHeight();
+        int draw_width=drawable.getIntrinsicWidth();
+        float rel=((float)width/(float)draw_width);
+        pantalla.set(0,0,width, (int)(draw_height*rel));
+        //Log.i("dim","w "+width);
+        //Log.i("dim","h "+(int)(height));
+       // Log.i("dim","dw "+draw_width);
+       // Log.i("dim","dh "+(int)(draw_height));
+        //Log.i("dim","rel "+ rel);
+        canvas.drawColor(0xFFFFFFFF);
+        drawable.setBounds(pantalla);
+        drawable2.setBounds(pantalla);
         drawable.draw(canvas);
         drawable2.draw(canvas);
 
