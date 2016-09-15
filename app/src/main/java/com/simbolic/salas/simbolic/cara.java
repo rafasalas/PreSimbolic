c4d09edf4b52b9da341310ffd2532853b8ea6b3e
salas <rafa.salas@gmail.com>
2016-09-15 13:49:35 +0200

    more elements

--- app/src/main/java/com/simbolic/salas/simbolic/cara.java
+++ app/src/main/java/com/simbolic/salas/simbolic/cara.java
@@ -10,7 +10,7 @@ import java.util.Random;
  * Created by salas on 27/07/2016.
  */
 public class cara {
-    partecara pelo,cejas,ojos, nariz, boca, mandibula;
+    partecara pelo,cejas,ojos, nariz, boca, mandibula, mano_izq, mano_der, cuello;
     public cara(Context context){
         pelo=new partecara(context);
         cejas=new partecara(context);
@@ -18,7 +18,9 @@ public class cara {
         nariz=new partecara(context);
         boca=new partecara(context);
         mandibula=new partecara(context);
-
+        mano_izq=new partecara(context);
+        mano_der=new partecara(context);
+        cuello=new partecara(context);
     }
 
     public void resize (int width, int height, double scale){
@@ -34,7 +36,9 @@ public class cara {
         boca.resize(width, height,scale);
 
         mandibula.resize(width, height,scale);
-
+        mano_izq.resize(width, height,scale);
+       mano_der.resize(width, height,scale);
+       cuello.resize(width, height,scale);
 
     }
 
@@ -43,37 +47,45 @@ public class cara {
 
         //Log.v("nombre", "hair_"+ Integer.toString(rnd.nextInt(5)));
 
-        pelo.loadsvg("hair_"+ Integer.toString(rnd.nextInt(5)));
+        pelo.loadsvg("hair_"+ Integer.toString(rnd.nextInt(9)));
 
-        cejas.loadsvg("eyebrow_"+ Integer.toString(rnd.nextInt(5)));
+        cejas.loadsvg("eyebrow_"+ Integer.toString(rnd.nextInt(9)));
 
-        ojos.loadsvg("eyes_"+ Integer.toString(rnd.nextInt(6)));
+        ojos.loadsvg("eye_"+ Integer.toString(rnd.nextInt(9)));
 
-        nariz.loadsvg("nose_"+ Integer.toString(rnd.nextInt(4)));
+        nariz.loadsvg("nose_"+ Integer.toString(rnd.nextInt(9)));
 
-        boca.loadsvg("mouth_"+ Integer.toString(rnd.nextInt(6)));
+        boca.loadsvg("mouth_"+ Integer.toString(rnd.nextInt(9)));
 
-        mandibula.loadsvg("faces_"+ Integer.toString(rnd.nextInt(5)));
-
+        mandibula.loadsvg("faces_"+ Integer.toString(rnd.nextInt(9)));
+        mano_der.loadsvg("hand_right_"+ Integer.toString(rnd.nextInt(3)));
+        mano_izq.loadsvg("hand_left_"+ Integer.toString(rnd.nextInt(9)));
+        cuello.loadsvg("neck_"+ Integer.toString(rnd.nextInt(1)));
     }
 
     public void dibujar(Canvas canvas){
-        mandibula.dibujar(canvas);
 
+        cuello.dibujar(canvas);
+        mandibula.dibujar(canvas);
         pelo.dibujar(canvas);
         cejas.dibujar(canvas);
         ojos.dibujar(canvas);
         nariz.dibujar(canvas);
         boca.dibujar(canvas);
+        mano_izq.dibujar(canvas);
+        mano_der.dibujar(canvas);
 
     }
     public void alfa(int op){
+        cuello.alfa(op);
         mandibula.alfa(op);
         pelo.alfa(op);
         cejas.alfa(op);
         ojos.alfa(op);
         nariz.alfa(op);
         boca.alfa(op);
+        mano_izq.alfa(op);
+        mano_der.alfa(op);
 
     }
 }
